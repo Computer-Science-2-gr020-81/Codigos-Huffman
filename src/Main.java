444e8593670c892c935fa1ead2dbea3569c7c7db
Andres Torres <cesarandrestorresbernal604@gmail.com>
2021-11-06 00:11:23 -0500

    adelanto interfaz y arbol

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
+import javax.swing.JFrame;
+import models.Arbol;
+import controllers.Controlador;
+import views.Lienzo;
 import views.Menu;
+import views.MenuN;
 
 public class Main {
     
     public static void main(String[] args) {
-        new Menu().start();
+      //  new Menu().start();
+       new MenuN().setVisible(true);
     }
+    
     
 }
--- src/controllers/DataController.java
+++ src/controllers/DataController.java
@@ -85,4 +85,5 @@ public class DataController {
         Util.printRepresentation(treeRepresentation);
     }
     
+    
 }
